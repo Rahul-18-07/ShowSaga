61c95177fbb208c320982ee4212566cdfd6efdba
Rahul <96981409+Rahul-18-07@users.noreply.github.com>
2024-02-29 20:21:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Appv1 from './Appv1';
+import App from './App';
 // import StarRating from './components/star';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
