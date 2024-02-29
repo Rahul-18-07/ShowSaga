429d963932e912b1f4008496b01bd3ae7ba557ff
Rahul <96981409+Rahul-18-07@users.noreply.github.com>
2024-02-29 20:20:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Appv1 from './Appv1';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Appv1 />
+    <App/>
   </React.StrictMode>
 );
 
